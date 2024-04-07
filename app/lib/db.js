de5e72915f774d3294fe5fefce093ad235082a06
alexander parco flores <alexander.parco@somosari.com>
2024-04-07 14:15:18 -0500

    fix build1

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,13 +1,26 @@
 import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-	return new PrismaClient()
+// const prismaClientSingleton = () => {
+// 	return new PrismaClient()
+// }
+
+// const globalForPrisma = globalThis
+
+// const prisma = globalForPrisma.prismaGlobal ?? prismaClientSingleton()
+
+// export default prisma
+
+// if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+
+let prisma
+
+if (process.env.NODE_ENV === 'production') {
+	prisma = new PrismaClient()
+} else {
+	if (!global.prisma) {
+		global.prisma = new PrismaClient()
+	}
+	prisma = global.prisma
 }
 
-const globalForPrisma = globalThis
-
-const prisma = globalForPrisma.prismaGlobal ?? prismaClientSingleton()
-
 export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
